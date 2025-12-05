6467f364e36318d953af8b2771e9367aa86a97a6
Volchonoc-cod <gv2843681@gmail.com>
2025-12-05 16:04:09 +0300

    Update __init__.py

--- web/handlers/__init__.py
+++ web/handlers/__init__.py
@@ -1,1 +1,29 @@
 
+"""
+Инициализация пакета обработчиков
+"""
+from .main import index_handler
+from .backups import backups_handler
+from .monitor import monitor_handler
+from .users import users_handler
+from .settings import settings_handler
+from .logs import logs_handler
+from .api import (
+    api_stats_handler,
+    api_system_stats_handler,
+    api_create_backup,
+    api_send_backup
+)
+
+__all__ = [
+    'index_handler',
+    'backups_handler', 
+    'monitor_handler',
+    'users_handler',
+    'settings_handler',
+    'logs_handler',
+    'api_stats_handler',
+    'api_system_stats_handler',
+    'api_create_backup',
+    'api_send_backup'
+]
